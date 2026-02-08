c76921fc79012717b28e3e4825d34ec680bf107d
evikshevchuk <shev555@inbox.ru>
2026-02-08 19:44:39 +0300

    feat: implement canvas based image slider

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+import CanvasSlider from './components/CanvasSlider.vue'
+</script>
+
+<template>
+  <main>
+    <CanvasSlider />
+  </main>
+</template>
+
+<style scoped>
+main {
+  margin: 40px auto;
+  padding: 0 16px;
+}
+</style>
